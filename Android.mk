f4d4c9cbd87f252080406ebada6668f210ce9df4
tezetzy <firlanrizqulloh@gmail.com>
2025-06-14 02:37:25 +0700

    Update and rename jni/Android.mk to Android.mk

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := modtemplate
-LOCAL_SRC_FILES := main.cpp mod/logger.cpp
+LOCAL_MODULE    := abtfixes
+LOCAL_SRC_FILES := main.cpp jni/mod/logger.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
 LOCAL_LDLIBS += -llog

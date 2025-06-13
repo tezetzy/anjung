61dace2aca4851351c92c046499d2c04db325127
tezetzy <firlanrizqulloh@gmail.com>
2025-06-14 05:57:46 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := AbtFixesJay
-LOCAL_SRC_FILES := AbtFixes.cpp
+LOCAL_MODULE := FixesJiggle
+LOCAL_SRC_FILES := main.cpp
 LOCAL_CPPFLAGS += -Wall -std=c++17
 LOCAL_LDLIBS := -llog -ldl
 include $(BUILD_SHARED_LIBRARY)
